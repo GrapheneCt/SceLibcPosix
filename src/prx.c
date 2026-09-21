344e7d1459e0ad1baee580ae05e6dfa241996563
GrapheneCt <57592952+GrapheneCt@users.noreply.github.com>
2026-09-21 05:32:40 +0200

    Make FIOS2 init optional

--- src/prx.c
+++ src/prx.c
@@ -2,14 +2,21 @@
 #include "vitafs.h"
 #include "vitamman.h"
 
-int module_start(int argc, const void *args) {
+int module_start(unsigned int args, const void *argp) {
+	unsigned int flags = 0;
+	if (args == 4 && argp) {
+		flags = *(unsigned int *)argp;
+	}
+
 	_init_fd();
-	__init_fios2();
+	if (!(flags & 1)) {
+		__init_fios2();
+	}
 	__init_mman();
 	// __init_threads();
 	return 0;
 }
-int module_stop(int argc, const void *args) {
+int module_stop(unsigned int args, const void *argp) {
 	__fini_mman();
 	_free_fd();
 	return 0;
